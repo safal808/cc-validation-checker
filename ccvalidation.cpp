767836316f9a233dbb2980ece7b3566e7c94b3e7
Safal <85111654+safal808@users.noreply.github.com>
2022-05-22 21:16:15 +0545

    Update ccvalidation.cpp

--- ccvalidation.cpp
+++ ccvalidation.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 bool isNString(const string& s) {
     int len = s.length();
     for (int i = 0; i < len; i++) {
